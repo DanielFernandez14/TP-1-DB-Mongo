b92d373e94eaa6ae83176af80112fda125006241
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-05-08 18:54:30 -0300

    Tercer B- Tp gamesSchema

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import { Schema, model, connect } from "mongoose";
-process.loadEnvFile()
+process.loadEnvFile();
 
-const URI_DB = process.env.URI_DB || ""
-
-
+const URI_DB = process.env.URI_DB || "";
 
 const connectMongoDb = async () => {
     try {
-        await connect(URI_DB)
-        console.log("Conectado a MongoDB")
+    await connect(URI_DB);
+    console.log("Conectado a MongoDB");
     } catch (error) {
-        console.log("Error al conectar a MongoDB")
+    console.log("Error al conectar a MongoDB");
     }
-}
+};
 
+const gamesSchema = new Schema({
+    title: { type: String, required: true, unique: true },
+    year: { type: Number, required: true, unique: true },
+    rating: { type: Number, default: 0 },
+    gender: { type: String, required: true },
+});
 
-
-
-
-connectMongoDb()
+connectMongoDb();
