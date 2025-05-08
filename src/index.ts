788bb7c80fdcef634d548cc629762fe8faa7a55d
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-05-08 18:47:31 -0300

    Second B- Tp Conexion a MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("probando")
+import { Schema, model, connect } from "mongoose";
+process.loadEnvFile()
+
+const URI_DB = process.env.URI_DB || ""
+
+
+
+const connectMongoDb = async () => {
+    try {
+        await connect(URI_DB)
+        console.log("Conectado a MongoDB")
+    } catch (error) {
+        console.log("Error al conectar a MongoDB")
+    }
+}
+
+
+
+
+
+connectMongoDb()
